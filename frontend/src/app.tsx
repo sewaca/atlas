abd591bc42bb5c2a2a1bb969ed093c186be5317f
sewaca <sewaca7@gmail.com>
2025-01-03 18:30:55 +0300

    убрал лишнее

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,14 +1,7 @@
-import { component$, useSignal } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 
 import "./app.css";
-import { Button } from "./components/Button/Button";
 
 export const App = component$(() => {
-  const count = useSignal(0);
-
-  return (
-    <>
-      <Button onClick={() => count.value++}> {count} </Button>
-    </>
-  );
+  return <></>;
 });
